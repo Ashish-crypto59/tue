2dd041cdbac900119a34ab8c5ee177aa1ee4d170
Ashish-crypto59 <as5515824@gmail.com>
2025-09-15 22:46:23 +0530

    Time: 0 ms (100%), Space: 10.8 MB (22.48%) - LeetHub

---
+++ LeetCode/Medium/0198-house-robber/0198-house-robber-09-15-2025-22-56-10.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int solve(vector<int>& nums, int size, int index, vector<int>& dp) {
+        if (index >= size) {
+            return 0;
+        }
+
+        if (dp[index] != -1) {
+            return dp[index];
+        }
+
+        int option1 = nums[index] + solve(nums, size, index + 2, dp);
+        int option2 = solve(nums, size, index + 1, dp);
+
+        dp[index] = max(option1, option2);
+        return dp[index];
+    }
+
+    int rob(vector<int>& nums) {
+        int size = nums.size();
+        vector<int> dp(size, -1);
+        return solve(nums, size, 0, dp);
+    }
+};
