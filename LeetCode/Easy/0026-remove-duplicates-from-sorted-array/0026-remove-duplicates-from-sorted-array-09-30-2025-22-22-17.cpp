7b5a74cc5ee0ccc01a9ec672c0d61df803ccf473
Ashish-crypto59 <as5515824@gmail.com>
2025-09-30 22:12:21 +0530

    Time: 0 ms (100%), Space: 22.5 MB (96.56%) - LeetHub

---
+++ LeetCode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array-09-30-2025-22-22-17.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+    if (nums.empty()) return 0;
+    int i = 0;
+    for (int j = 1; j < nums.size(); j++) {
+        if (nums[j] != nums[i]) {
+            i++;  
+            nums[i] = nums[j]; 
+        }
+    }
+    return i + 1; 
+}
+
+int main() {
+    vector<int> nums1 = {1, 1, 2};
+    int k1 = removeDuplicates(nums1);
+    cout << "k = " << k1 << ", nums = [";
+    for (int i = 0; i < k1; i++) {
+        cout << nums1[i] << (i < k1 - 1 ? ", " : "");
+    }
+    cout << ", ...]" << endl;
+
+    vector<int> nums2 = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+    int k2 = removeDuplicates(nums2);
+    cout << "k = " << k2 << ", nums = [";
+    for (int i = 0; i < k2; i++) {
+        cout << nums2[i] << (i < k2 - 1 ? ", " : "");
+    }
+    cout << ", ...]" << endl;
+
+    return 0;
+  }     
+};
