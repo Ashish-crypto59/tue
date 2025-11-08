cf994e4d4e4d3d1f30190087ce99489a85aee15b
Ashish-crypto59 <as5515824@gmail.com>
2025-11-09 00:49:18 +0530

    Time: 0 ms (100%), Space: 8.5 MB (90%) - LeetHub

---
+++ LeetCode/Easy/0392-is-subsequence/0392-is-subsequence-11-09-2025-00-59-46.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int sl=0;
+        int tl=0;
+        while(sl<s.length() && tl<t.length()){
+            if(s[sl]==t[tl]){
+                sl++;
+            }
+            tl++;
+        }
+        return sl==s.length();
+    }
+};
