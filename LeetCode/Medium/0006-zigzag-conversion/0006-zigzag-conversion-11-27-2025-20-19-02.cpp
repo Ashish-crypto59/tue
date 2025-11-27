772dd0ce65c720a5f67f413877b533ccc5179681
Ashish-crypto59 <as5515824@gmail.com>
2025-11-27 20:08:21 +0530

    Time: 7 ms (45.36%), Space: 14.1 MB (52.2%) - LeetHub

---
+++ LeetCode/Medium/0006-zigzag-conversion/0006-zigzag-conversion-11-27-2025-20-19-02.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+      if (numRows == 1 || numRows >= s.size()) {
+            return s;
+        }
+        vector<string> rows(numRows);
+        int index = 0;
+        int step = -1;
+        for (char c : s) {
+            rows[index] += c;
+            if (index == 0) {
+                step = 1;
+            } else if (index == numRows - 1) {
+                step = -1;
+            }
+            index += step;
+        } 
+        string result;
+        for (string row : rows) {
+            result += row;
+        }
+        return result;
+    }
+};
