98448994c3087148c239a62fbd25cb271a4373b5
Ashish-crypto59 <as5515824@gmail.com>
2025-11-30 10:24:23 +0530

    Time: 0 ms (100%), Space: 8.6 MB (81.13%) - LeetHub

---
+++ LeetCode/Easy/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string-11-30-2025-10-35-06.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+    if (needle.empty()) {
+            return 0;
+     } 
+        int haystackLen = haystack.size();
+        int needleLen = needle.size();
+        for (int i = 0; i <= haystackLen - needleLen; i++) {
+            if (haystack.substr(i, needleLen) == needle) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
